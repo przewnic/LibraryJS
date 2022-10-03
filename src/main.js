a6427ed0c80ec53e21882fbee4ae945bfc516914
przewnic <24957921+przewnic@users.noreply.github.com>
2022-10-03 19:24:18 +0200

    Add form validation

--- src/main.js
+++ src/main.js
@@ -9,22 +9,47 @@ const submitButton = document.getElementById('submit-bttn');
 const addButton = document.getElementById('add-bttn');
 const cancelButton = document.getElementById('cancel-bttn');
 
-function addBook(author, title, pages, isRead) {
-  const newBook = myLibrary.addBook(author, title, pages, isRead);
+// Form validity
+const title = document.getElementById('title');
+const titleError = document.querySelector('#title + span.error');
+const author = document.getElementById('author');
+const authorError = document.querySelector('#author + span.error');
+const pages = document.getElementById('pages');
+const pagesError = document.querySelector('#pages + span.error');
+
+function addBook(authorValue, titleValue, pagesValue, isReadValue) {
+  const newBook = myLibrary.addBook(authorValue, titleValue, pagesValue, isReadValue);
   const card = createCard(newBook, myLibrary);
   cardsContainer.appendChild(card);
 }
 
+// Clear form errors
+function clearInputErrors(inputError) {
+  inputError.textContent = '';
+  inputError.className = 'error';
+}
+
+function clearForm() {
+  clearInputErrors(titleError);
+  clearInputErrors(authorError);
+  clearInputErrors(pagesError);
+  addForm.reset();
+  title.className = 'init';
+  author.className = 'init';
+  pages.className = 'init';
+}
+
 function hideForm() {
   formContainer.style.visibility = 'hidden';
+  clearForm();
 }
 
 function submitButtonAction(e) {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const isRead = document.getElementById('read-status').checked;
-  addBook(author, title, pages, isRead);
+  const titleValue = document.getElementById('title').value;
+  const authorValue = document.getElementById('author').value;
+  const pagesValue = document.getElementById('pages').value;
+  const isReadValue = document.getElementById('read-status').checked;
+  addBook(authorValue, titleValue, pagesValue, isReadValue);
   hideForm(e);
 }
 
@@ -33,10 +58,47 @@ function displayFormAction() {
   formContainer.style.visibility = 'visible';
 }
 
-submitButton.addEventListener('click', submitButtonAction);
+submitButton.addEventListener('click', (event) => { if (addForm.checkValidity()) { submitButtonAction(); } });
 addButton.addEventListener('click', displayFormAction);
 cancelButton.addEventListener('click', hideForm);
 hideForm(); // Hide the add book form initially
+
+// Handling form errors
+title.addEventListener('input', (event) => {
+  if (title.validity.valid) {
+    clearInputErrors(titleError);
+  } else if (title.validity.valueMissing) {
+    titleError.textContent = 'You need to enter a book title.';
+    titleError.className = 'error active';
+  } else if (title.validity.tooShort) {
+    titleError.textContent = 'Min length = 2.';
+    titleError.className = 'error active';
+  }
+  title.className = '';
+});
+
+author.addEventListener('input', (event) => {
+  if (author.validity.valid) {
+    clearInputErrors(authorError);
+  } else if (author.validity.valueMissing) {
+    authorError.textContent = 'You need to enter an author.';
+    authorError.className = 'error active';
+  } else if (author.validity.tooShort) {
+    authorError.textContent = 'Min length = 5.';
+    authorError.className = 'error active';
+  }
+  author.className = '';
+});
+
+pages.addEventListener('input', (event) => {
+  if (pages.validity.valid) {
+    clearInputErrors(pagesError);
+  } else if (pages.validity.valueMissing) {
+    pagesError.textContent = 'You need to enter book\'s number of pages.';
+    pagesError.className = 'error active';
+  }
+  pages.className = '';
+});
 
 addBook('Tolkien', 'The Lord of the Rings', 500, false);
 addBook('Tolkien', 'Hobbit', 310, false);
